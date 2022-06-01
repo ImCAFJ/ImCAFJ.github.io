3cf7c582f2489247739d7acdb7d6fd9316958aa7
Carlos Alberto <cafj100@gmail.com>
2022-06-01 15:53:47 -0300

    teste commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Routes from './Routes';  
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <Routes />
+    </>
+  );
+}
+
+export default App;
--- src/components/About/AboutMeCards/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Container = styled.div`
-    display: grid;
-    gap: 1rem;
-
-    margin-top: 2rem;
-`
